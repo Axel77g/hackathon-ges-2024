1817e98dee2d6403b2a5bdb0f9c2acce4c53b83b
Abd <abdoulayediallo75017@outlook.fr>
2024-04-25 10:59:42 +0200

    - Voila pour toi Gary

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ',WA.player.tags);
+    WA.player.tags.push('subscribed_1000');
+
+    console.log(WA.room);
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -17,7 +20,25 @@ WA.onInit().then(() => {
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(closePopup);
+
+    WA.room.area.onEnter('connexion_popup').subscribe(() => {
+        if(!WA.player.tags.includes("subscribed_1000")) {
+            currentPopup = WA.ui.openPopup("message", "Vous devez vous être connecté pour entrer", []);
+        }
+    })
+    WA.room.area.onLeave('connexion_popup').subscribe(closePopup);
+
+    WA.room.onEnterLayer('openDoorZone').subscribe(() => {
+        if(WA.player.tags.includes('subscribed_1000')) {
+            WA.room.showLayer('above/openDoor');
+            WA.room.hideLayer('closeDoor');
+        } else {
+            WA.room.hideLayer('above/openDoor');
+            WA.room.showLayer('closeDoor');
+        }
+    })
+    
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
